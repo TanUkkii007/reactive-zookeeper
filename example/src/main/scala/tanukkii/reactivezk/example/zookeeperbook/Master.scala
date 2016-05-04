d3299713d40d48e709c450ededaebf1786ad63c1
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-05-04 21:46:18 +0900

    add masterExists

--- example/src/main/scala/tanukkii/reactivezk/example/zookeeperbook/Master.scala
+++ example/src/main/scala/tanukkii/reactivezk/example/zookeeperbook/Master.scala
@@ -3,7 +3,9 @@ package tanukkii.reactivezk.example.zookeeperbook
 import akka.actor.{ActorLogging, Props, ActorRef, Actor}
 import org.apache.zookeeper.CreateMode
 import org.apache.zookeeper.KeeperException.Code
+import org.apache.zookeeper.Watcher.Event.EventType
 import org.apache.zookeeper.ZooDefs.Ids
+import tanukkii.reactivezk.ZooKeeperActorProtocol.ZooKeeperWatchEvent
 import tanukkii.reactivezk.{IdConversions, ZKOperations}
 import scala.concurrent.duration._
 import IdConversions._
@@ -19,6 +21,7 @@ object MasterStates {
 object MasterProtocol {
   case object RunForMaster
   case object CheckMaster
+  case object MasterExists
   case class MasterElectionEnd(status: MasterState)
 }
 
@@ -38,11 +41,15 @@ class Master(serverId: String, zookeeperSession: ActorRef, supervisor: ActorRef)
     }
     case Created(path, name) => {
       state = MasterStates.Elected
+      log.info(s"I'm the leader $serverId")
       supervisor ! MasterElectionEnd(state)
     }
     case CreateFailure(e) if e.code() == Code.NODEEXISTS => {
       state = MasterStates.NotElected
+      log.info(s"I'm not the leader $serverId")
       supervisor ! MasterElectionEnd(state)
+      context.become(masterExists)
+      self ! MasterExists
     }
     case CreateFailure(e) if e.code() == Code.CONNECTIONLOSS => {
       context.become(checkMaster)
@@ -52,7 +59,7 @@ class Master(serverId: String, zookeeperSession: ActorRef, supervisor: ActorRef)
   }
 
   def checkMaster: Receive = {
-    case CheckMaster => zookeeperSession ! GetData("/master", false)
+    case CheckMaster => zookeeperSession ! GetData("/master")
     case DataGot(path, data, stat) => {
       if (data.toString == serverId) {
         state = MasterStates.Elected
@@ -70,6 +77,25 @@ class Master(serverId: String, zookeeperSession: ActorRef, supervisor: ActorRef)
       context.system.scheduler.scheduleOnce(1 second, self, CheckMaster)
     }
     case GetDataFailure(e) => throw e
+  }
+
+  def masterExists: Receive = {
+    case MasterExists => zookeeperSession ! Exists("/master", watch = true)
+    case DoesExist(path, stat) =>
+    case ExistsFailure(e) if e.code() == Code.CONNECTIONLOSS => {
+      self ! MasterExists
+    }
+    case ExistsFailure(e) if e.code() == Code.NONODE => {
+      state = MasterStates.Running
+      context.become(runForMaster)
+      self ! RunForMaster
+      log.info("It sounds like the previous master is gone, so let's run for master again.")
+    }
+    case ZooKeeperWatchEvent(e) if e.getType == EventType.NodeDeleted => {
+      assert("/master" == e.getPath)
+      context.become(runForMaster)
+      self ! RunForMaster
+    }
   }
 }
 
