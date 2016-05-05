ccf843b0a467f322b8e4f0bfbca0d621c6f999bb
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-05-05 11:20:24 +0900

    bugfix: null check when access to children in getChildren

--- reactive-zookeeper/src/main/scala/tanukkii/reactivezk/ChildrenCallbackConversion.scala
+++ reactive-zookeeper/src/main/scala/tanukkii/reactivezk/ChildrenCallbackConversion.scala
@@ -5,7 +5,9 @@ import scala.collection.JavaConverters._
 
 trait ChildrenCallbackConversion {
   implicit def toChildrenCallback[Ctx](f: (Int, String, Ctx, List[String]) => Unit): ChildrenCallback = new ChildrenCallback {
-    def processResult(rc: Int, path: String, ctx: Any, children: java.util.List[String]): Unit = f(rc, path, ctx.asInstanceOf[Ctx], children.asScala.toList)
+    def processResult(rc: Int, path: String, ctx: Any, children: java.util.List[String]): Unit = {
+      f(rc, path, ctx.asInstanceOf[Ctx], Option(children).fold(List.empty[String])(_.asScala.toList)) //children may be null
+    }
   }
 }
 
