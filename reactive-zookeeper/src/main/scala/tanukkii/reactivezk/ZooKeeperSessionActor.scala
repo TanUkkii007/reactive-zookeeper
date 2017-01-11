7b46a56ebc495a1710145d8ad0f0bcc62ed3e290
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2017-01-11 13:56:54 +0900

    restart ZooKeeper session on session expiration

--- reactive-zookeeper/src/main/scala/tanukkii/reactivezk/ZooKeeperSessionActor.scala
+++ reactive-zookeeper/src/main/scala/tanukkii/reactivezk/ZooKeeperSessionActor.scala
@@ -14,7 +14,7 @@ object ZooKeeperSession {
   case object Restarted
 }
 
-@SerialVersionUID(1L) case class ZooKeeperSessionRestartException(sender: ActorRef) extends Exception("closing the ZooKeeper session and will reestablish a new session")
+@SerialVersionUID(1L) case class ZooKeeperSessionRestartException(sender: Option[ActorRef]) extends Exception("closing the ZooKeeper session and will reestablish a new session")
 
 private [reactivezk] class ZooKeeperSessionActor(connectString: String, sessionTimeout: FiniteDuration) extends Actor
 with ActorLogging with WatcherCallback{
@@ -22,12 +22,11 @@ with ActorLogging with WatcherCallback{
   import ZooKeeperSession._
 
   var connected = false
-  var expired = false
   var closed = false
 
   val zookeeper = new ZooKeeper(connectString, sessionTimeout.toMillis.toInt, watchCallback(self))
 
-  val zookeeperOperation = context.actorOf(ZooKeeperOperationActor.props(zookeeper), "zookeeper-operation")
+  val zookeeperOperation: ActorRef = context.actorOf(ZooKeeperOperationActor.props(zookeeper), "zookeeper-operation")
 
   def receive: Receive = {
     case ZooKeeperWatchEvent(e) => {
@@ -35,10 +34,9 @@ with ActorLogging with WatcherCallback{
       e.getState match {
         case SyncConnected => connected = true
         case Disconnected => connected = false
-        case Expired => connected = false; expired = true
+        case Expired => throw ZooKeeperSessionRestartException(None)
         case _ =>
       }
-      context.system.eventStream.publish(ZooKeeperWatchEvent(e))
     }
     case Close => {
       close()
@@ -46,7 +44,7 @@ with ActorLogging with WatcherCallback{
       sender() ! Closed
       context.stop(self)
     }
-    case Restart => throw new ZooKeeperSessionRestartException(sender())
+    case Restart => throw ZooKeeperSessionRestartException(Some(sender()))
     case other => zookeeperOperation forward other
   }
 
@@ -57,14 +55,14 @@ with ActorLogging with WatcherCallback{
 
   override def postRestart(reason: Throwable): Unit = {
     reason match {
-      case ZooKeeperSessionRestartException(ref) =>
+      case ZooKeeperSessionRestartException(Some(ref)) =>
         ref ! Restarted
       case _ =>
     }
     super.postRestart(reason)
   }
 
-  def close() = {
+  private[this] def close(): Unit = {
     zookeeper.close()
     log.info("ZooKeeper session is closed.")
   }
